7bcf6e83fd1be72867af68f9c365dd463a349f7a
Janith <janith@yamu.lk>
2015-06-26 18:19:57 +0530

    switched frontend from ratchet to bootstrap

--- static/js/app.js
+++ static/js/app.js
@@ -20,14 +20,14 @@ var Messages = React.createClass({
     setInterval(this.loadMessagesFromServer, this.props.pollInterval);
   },
   render: function() {
+    var clock = new Date().getTime();
     var messageNodes = this.state.messages.map(function(message, index) {
-      return <MessageNode message={message}/>
+      return <MessageNode message={message} clock={clock}/>
     });
     return (
-      <ul className="table-view">
-        <li className="table-view-divider">Recommended movies</li>
+      <div>
         {messageNodes}
-      </ul>
+      </div>
     );
   }
 });
@@ -35,20 +35,33 @@ var Messages = React.createClass({
 var MessageNode = React.createClass({
   render: function() {
     return(
-      <li className="table-view-cell media">
-        <a className="navigate" href="choose-theater.html" data-transition="slide-in">
-          <img className="media-object pull-left" src="http://placehold.it/64x64" alt="Placeholder image"/>
-          <div className="media-body">
-            {this.props.message[1]}
-            <p><small>{new Date(this.props.message[2]).toString()}</small></p>
-          </div>
-        </a>
-      </li>
+      <div className="panel panel-default">
+        <div className="panel-body">{this.props.message[1]}</div>
+        <div className="panel-footer">
+          <img className="message-image" src="http://placehold.it/15x15"/> 
+          <strong>Anonymous</strong> <Timestamp clock={this.props.clock} ts={this.props.message[2]}/>
+        </div>
+      </div>
     );
+  }
+});
+
+var Timestamp = React.createClass({
+  render: function() {
+    var seconds = Math.ceil((this.props.clock - this.props.ts) / 1000);
+    if(seconds > 3600) {
+      return (<small>({Math.floor(seconds / 3600)} hours ago)</small>);
+    }
+    else if(seconds > 60) {
+      return (<small>({Math.floor(seconds / 60)} minutes ago)</small>);
+    }
+    if(seconds > 3600) {
+      return (<small>({seconds} seconds ago)</small>);
+    }
   }
 });
 
 var app = React.render(
   <Messages url="./api/public/getmessages/10/" pollInterval={30000} />,
-  document.getElementById('main-content')
+  document.getElementById('message-thread')
 );
