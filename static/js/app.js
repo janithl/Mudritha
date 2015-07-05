b480f8ba92e98f414d273cae0fd9e0818fd42139
Janith <janith@yamu.lk>
2015-07-05 09:13:05 +0530

    moved message insertion into react and fixed bugs

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,4 @@
-var Messages = React.createClass({
+var Mudritha = React.createClass({
 	loadMessagesFromServer: function() {
 		$.ajax({
 			url: './api/public/getmessages/0/',
@@ -20,16 +20,41 @@ var Messages = React.createClass({
 	},
 	componentDidMount: function() {
 		this.loadMessagesFromServer();
-		setInterval(this.loadMessagesFromServer, this.props.pollInterval);
+		setInterval(this.loadMessagesFromServer, 30000);
 	},
 	render: function() {
+		return (
+			<div className="container-fluid">
+				<div className="col-md-3 col-xs-12">
+					<h4>Popular</h4>
+					<PopTerms />
+				</div>
+				<div className="col-md-6 col-xs-12">
+					<form>
+						<div className="form-group">
+							<label>Link URL</label>
+							<input type="text" className="form-control" placeholder="Link URL" />
+						</div>
+						<button type="button" className="btn btn-default">Submit</button>
+					</form>
+				</div>
+				<div className="col-md-3 col-xs-12">
+					<MessageInput onChange={this.loadMessagesFromServer}/>
+					<Messages messages={this.state.messages} />
+				</div>
+			</div>
+		);
+	},
+});
+
+var Messages = React.createClass({
+	render: function() {
 		var clock = new Date().getTime();
-		var messageNodes = this.state.messages.map(function(message, index) {
+		var messageNodes = this.props.messages.map(function(message, index) {
 			return <MessageNode message={message} clock={clock}/>
 		});
 		return (
-			<div>
-				<PopTerms />
+			<div className="message-thread">
 				{messageNodes}
 			</div>
 		);
@@ -62,6 +87,35 @@ var Timestamp = React.createClass({
 		else {
 			return (<small>({seconds} seconds ago)</small>);
 		}
+	}
+});
+
+var MessageInput = React.createClass({
+	getInitialState: function() {
+		return {text: ''};
+	},
+	handleChange: function(event) {
+		this.setState({text: event.target.value});
+	},
+	onKeyDown: function(event) {
+		if(event.which === 13 && !event.shiftKey) {
+			$.ajax({
+				url: './api/public/addmessage/',
+				dataType: 'text',
+				type: 'POST',
+				data: {text: this.state.text},
+				success: function(data) {
+					this.props.onChange();
+					this.setState({text: ''});
+				}.bind(this)
+			});
+		}
+	},
+	render: function() {
+		return (
+			<textarea className="form-control message-input" placeholder="Type here..." 
+			rows="3" onChange={this.handleChange} onKeyDown={this.onKeyDown} value={this.state.text}/>
+		);
 	}
 });
 
@@ -109,6 +163,6 @@ var TermNode = React.createClass({
 });
 
 var app = React.render(
-	<Messages pollInterval={30000} />,
-	document.getElementById('message-thread')
+	<Mudritha />,
+	document.getElementById('main-content')
 );
